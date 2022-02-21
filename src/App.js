8b57a30efbc2d34d61fd0c3a735a7e311e2400a1
mrksays <mrksays@gmail.com>
2022-02-21 15:30:40 +0500

    mustafa changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       {/* <Switch>
-        <Route path='/' component={Home} exact Rameez />
+        <Route path='/' component={Home} exact Mustafa />
         <Route path='/signin' component={SigninPage} exact />
       </Switch> */}
       <Home />
